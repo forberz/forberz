5794a12d6adbf90d6ab3f25b32085eaf82c5806e
freaker2k7 <thenetfreaker@gmail.om>
2018-06-21 13:42:31 +0300

    Set disqus to page

--- site/disqus.php
+++ site/disqus.php
@@ -5,12 +5,12 @@
 /**
 *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
 *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
-/*
+
 var disqus_config = function () {
-this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
-this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+	this.page.url = document.location.href;  // Replace PAGE_URL with your page's canonical URL variable
+	this.page.identifier = document.getElementsByTagName('title')[0].innerText; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
 };
-*/
+
 
 var disqus_config = function () { 
 	this.language = LANG;
